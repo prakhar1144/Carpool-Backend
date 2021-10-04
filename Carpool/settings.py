2dac2258df4bd3807ee73ebdcc76d6ec10c49d6a
prakhar <prakhar841301@gmail.com>
2021-10-04 12:49:08 +0530

    static root required in production

--- Carpool/settings.py
+++ Carpool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # pip install django-environ
 import environ
@@ -148,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
