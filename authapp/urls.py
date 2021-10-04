e40d835b3e2d5bd45d6fc2a714b8a8f512805fd0
prakhar <prakhar841301@gmail.com>
2021-10-05 02:53:04 +0530

    create instead of ' '

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterAPIView, VerifyEmailAPIView, LoginAPIView, ForgotPasswordAPIView
 
 urlpatterns = [
-    path('', RegisterAPIView.as_view()),
+    path('register/', RegisterAPIView.as_view()),
     path('verify/', VerifyEmailAPIView.as_view() ,name="mail-verification"),
     path('login/', LoginAPIView.as_view()),
     path('reset/', ForgotPasswordAPIView.as_view()),
