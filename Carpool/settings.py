9e9e253755ec1d27afd37d7dd2248bacca59a81b
prakhar <prakhar841301@gmail.com>
2021-10-04 12:29:09 +0530

    will set allowed_host using env variable

--- Carpool/settings.py
+++ Carpool/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('ALLOWED_HOST')]
 
 
 # Application definition
