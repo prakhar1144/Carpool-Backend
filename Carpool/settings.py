693780d889052c90f0f4e7321b36deaed5d28eb6
prakhar <prakhar841301@gmail.com>
2021-10-27 00:09:42 +0530

    trying to fix redis setting

--- Carpool/settings.py
+++ Carpool/settings.py
@@ -95,7 +95,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [env('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
