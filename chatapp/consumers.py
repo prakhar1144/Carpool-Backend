bd464f43eeab468ca2f394f9670c6df55aac95f6
prakhar <prakhar841301@gmail.com>
2021-11-06 17:27:41 +0530

    send mssg + name both

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -26,21 +26,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        name = text_data_json['name']
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'name':name
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        name = event['name']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'name': name
         }))
