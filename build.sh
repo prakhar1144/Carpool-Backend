a21e6cfe71ffe11b0dd30e38cebe56e857b64d60
Prakhar Pratyush <prakhar841301@gmail.com>
2023-01-26 16:17:48 +0530

    Configs: Update static files config and build script.
    build script was not properly written to install requirements
    file packages.
    
    build script now also includes command to create super user
    , for the first time.
    
    Static les related settings updated following render docs.
    
    This commit was amended to createsuperuser during deployment
    on Jul 6.
    
    First time this commit was created on Jan 26.
    
    Not adding separate commits because it doesn't adds much value
    and spam commits.

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# This command is needed only once.
+# python manage.py createsuperuser --email prakhar1144@gmail.com --no-input
