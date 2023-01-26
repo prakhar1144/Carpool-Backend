da4f7b186d129fc9c6f59d5eb1e479de83fce387
Prakhar Pratyush <prakhar841301@gmail.com>
2023-01-26 14:11:28 +0530

    Deploy: Manual deployment configurations for render.

--- Carpool/settings.py
+++ Carpool/settings.py
@@ -34,7 +34,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env('ALLOWED_HOST')]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
